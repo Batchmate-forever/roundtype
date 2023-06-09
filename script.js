e07eaab1d433d2b614a7129a5b5a5bbdc83493a8
Batchmate-forever <135971074+Batchmate-forever@users.noreply.github.com>
2023-06-09 01:02:20 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,375 @@
+let section1Boys = [
+  {name : "Name of Student", pic : "person-icon.webp"},
+  {name : "Name of Student", pic : "person-icon.webp"},
+  {name : "Name of Student", pic : "person-icon.webp"},
+  {name : "Name of Student", pic : "person-icon.webp"},
+  {name : "Name of Student", pic : "person-icon.webp"},
+  {name : "Name of Student", pic : "person-icon.webp"},
+  {name : "Name of Student", pic : "person-icon.webp"},
+  {name : "Name of Student", pic : "person-icon.webp"},
+  {name : "Name of Student", pic : "person-icon.webp"},
+  {name : "Name of Student", pic : "person-icon.webp"}
+];
+let section1Girls = [
+  {name : "Name of Student", pic : "woman-icon.jpg"},
+  {name : "Name of Student", pic : "woman-icon.jpg"},
+  {name : "Name of Student", pic : "woman-icon.jpg"},
+  {name : "Name of Student", pic : "woman-icon.jpg"},
+  {name : "Name of Student", pic : "woman-icon.jpg"},
+  {name : "Name of Student", pic : "woman-icon.jpg"},
+  {name : "Name of Student", pic : "woman-icon.jpg"},
+  {name : "Name of Student", pic : "woman-icon.jpg"},
+  {name : "Name of Student", pic : "woman-icon.jpg"},
+  {name : "Name of Student", pic : "woman-icon.jpg"}
+];
+let section2Boys = [
+  {name : "Name of Student", pic : "person-icon.webp"},
+  {name : "Name of Student", pic : "person-icon.webp"},
+  {name : "Name of Student", pic : "person-icon.webp"},
+  {name : "Name of Student", pic : "person-icon.webp"},
+  {name : "Name of Student", pic : "person-icon.webp"},
+  {name : "Name of Student", pic : "person-icon.webp"},
+  {name : "Name of Student", pic : "person-icon.webp"},
+  {name : "Name of Student", pic : "person-icon.webp"},
+  {name : "Name of Student", pic : "person-icon.webp"},
+  {name : "Name of Student", pic : "person-icon.webp"}
+];
+let section2Girls = [
+  {name : "Name of Student", pic : "woman-icon.jpg"},
+  {name : "Name of Student", pic : "woman-icon.jpg"},
+  {name : "Name of Student", pic : "woman-icon.jpg"},
+  {name : "Name of Student", pic : "woman-icon.jpg"},
+  {name : "Name of Student", pic : "woman-icon.jpg"},
+  {name : "Name of Student", pic : "woman-icon.jpg"},
+  {name : "Name of Student", pic : "woman-icon.jpg"},
+  {name : "Name of Student", pic : "woman-icon.jpg"},
+  {name : "Name of Student", pic : "woman-icon.jpg"},
+  {name : "Name of Student", pic : "woman-icon.jpg"}
+];
+let section3Boys = [
+  {name : "Name of Student", pic : "person-icon.webp"},
+  {name : "Name of Student", pic : "person-icon.webp"},
+  {name : "Name of Student", pic : "person-icon.webp"},
+  {name : "Name of Student", pic : "person-icon.webp"},
+  {name : "Name of Student", pic : "person-icon.webp"},
+  {name : "Name of Student", pic : "person-icon.webp"},
+  {name : "Name of Student", pic : "person-icon.webp"},
+  {name : "Name of Student", pic : "person-icon.webp"},
+  {name : "Name of Student", pic : "person-icon.webp"},
+  {name : "Name of Student", pic : "person-icon.webp"}
+];
+let section3Girls = [
+  {name : "Name of Student", pic : "woman-icon.jpg"},
+  {name : "Name of Student", pic : "woman-icon.jpg"},
+  {name : "Name of Student", pic : "woman-icon.jpg"},
+  {name : "Name of Student", pic : "woman-icon.jpg"},
+  {name : "Name of Student", pic : "woman-icon.jpg"},
+  {name : "Name of Student", pic : "woman-icon.jpg"},
+  {name : "Name of Student", pic : "woman-icon.jpg"},
+  {name : "Name of Student", pic : "woman-icon.jpg"},
+  {name : "Name of Student", pic : "woman-icon.jpg"},
+  {name : "Name of Student", pic : "woman-icon.jpg"}
+];
+let section4Boys = [
+  {name : "Name of Student", pic : "person-icon.webp"},
+  {name : "Name of Student", pic : "person-icon.webp"},
+  {name : "Name of Student", pic : "person-icon.webp"},
+  {name : "Name of Student", pic : "person-icon.webp"},
+  {name : "Name of Student", pic : "person-icon.webp"},
+  {name : "Name of Student", pic : "person-icon.webp"},
+  {name : "Name of Student", pic : "person-icon.webp"},
+  {name : "Name of Student", pic : "person-icon.webp"},
+  {name : "Name of Student", pic : "person-icon.webp"},
+  {name : "Name of Student", pic : "person-icon.webp"}
+];
+let section4Girls = [
+  {name : "Name of Student", pic : "woman-icon.jpg"},
+  {name : "Name of Student", pic : "woman-icon.jpg"},
+  {name : "Name of Student", pic : "woman-icon.jpg"},
+  {name : "Name of Student", pic : "woman-icon.jpg"},
+  {name : "Name of Student", pic : "woman-icon.jpg"},
+  {name : "Name of Student", pic : "woman-icon.jpg"},
+  {name : "Name of Student", pic : "woman-icon.jpg"},
+  {name : "Name of Student", pic : "woman-icon.jpg"},
+  {name : "Name of Student", pic : "woman-icon.jpg"},
+  {name : "Name of Student", pic : "woman-icon.jpg"}
+];
+let section5Boys = [
+  {name : "Name of Student", pic : "person-icon.webp"},
+  {name : "Name of Student", pic : "person-icon.webp"},
+  {name : "Name of Student", pic : "person-icon.webp"},
+  {name : "Name of Student", pic : "person-icon.webp"},
+  {name : "Name of Student", pic : "person-icon.webp"},
+  {name : "Name of Student", pic : "person-icon.webp"},
+  {name : "Name of Student", pic : "person-icon.webp"},
+  {name : "Name of Student", pic : "person-icon.webp"},
+  {name : "Name of Student", pic : "person-icon.webp"},
+  {name : "Name of Student", pic : "person-icon.webp"}
+];
+let section5Girls = [
+  {name : "Name of Student", pic : "woman-icon.jpg"},
+  {name : "Name of Student", pic : "woman-icon.jpg"},
+  {name : "Name of Student", pic : "woman-icon.jpg"},
+  {name : "Name of Student", pic : "woman-icon.jpg"},
+  {name : "Name of Student", pic : "woman-icon.jpg"},
+  {name : "Name of Student", pic : "woman-icon.jpg"},
+  {name : "Name of Student", pic : "woman-icon.jpg"},
+  {name : "Name of Student", pic : "woman-icon.jpg"},
+  {name : "Name of Student", pic : "woman-icon.jpg"},
+  {name : "Name of Student", pic : "woman-icon.jpg"}
+];
+let section6Boys = [
+  {name : "Name of Student", pic : "person-icon.webp"},
+  {name : "Name of Student", pic : "person-icon.webp"},
+  {name : "Name of Student", pic : "person-icon.webp"},
+  {name : "Name of Student", pic : "person-icon.webp"},
+  {name : "Name of Student", pic : "person-icon.webp"},
+  {name : "Name of Student", pic : "person-icon.webp"},
+  {name : "Name of Student", pic : "person-icon.webp"},
+  {name : "Name of Student", pic : "person-icon.webp"},
+  {name : "Name of Student", pic : "person-icon.webp"},
+  {name : "Name of Student", pic : "person-icon.webp"}
+];
+
+let section6Girls = [
+  {name : "Name of Student", pic : "woman-icon.jpg"},
+  {name : "Name of Student", pic : "woman-icon.jpg"},
+  {name : "Name of Student", pic : "woman-icon.jpg"},
+  {name : "Name of Student", pic : "woman-icon.jpg"},
+  {name : "Name of Student", pic : "woman-icon.jpg"},
+  {name : "Name of Student", pic : "woman-icon.jpg"},
+  {name : "Name of Student", pic : "woman-icon.jpg"},
+  {name : "Name of Student", pic : "woman-icon.jpg"},
+  {name : "Name of Student", pic : "woman-icon.jpg"},
+  {name : "Name of Student", pic : "woman-icon.jpg"}
+];
+
+let teachers = [
+  {name : "Name of Student", pic : "person-icon.webp"},
+  {name : "Name of Student", pic : "woman-icon.jpg"},
+  {name : "Name of Student", pic : "person-icon.webp"},
+  {name : "Name of Student", pic : "woman-icon.jpg"},
+  {name : "Name of Student", pic : "person-icon.webp"},
+  {name : "Name of Student", pic : "woman-icon.jpg"},
+  {name : "Name of Student", pic : "person-icon.webp"},
+  {name : "Name of Student", pic : "woman-icon.jpg"},
+  {name : "Name of Student", pic : "person-icon.webp"},
+  {name : "Name of Student", pic : "woman-icon.jpg"}
+];
+
+let memories = [
+  "memoy1.jpg","memory2.jpg","memory3.jpg","memory4.jpg","memory5.jpg","memory6.jpg","memory7.jpg","memory8.jpg","memory9.jpg","memory10.jpg","memory11.jpg","memory12.jpg","memory13.jpg","memory14.jpg","memory15.jpg","memory16.jpg","memory17.jpg","memory18.jpg","memory19.jpg","memory20.jpg"
+];
+
+let sections = [
+  "section1Boys","section1Girls","section2Boys","section2Girls","section3Boys","section3Girls","section4Boys","section4Girls","section5Boys","section5Girls","section6Boys","section6Girls",
+];
+
+let html = "";
+let todo = "";
+
+
+function displayStudent(setName, setpic, folder) {
+  todo = `<div>
+    <div class="child-student-container">
+      <img src="${folder}/${setpic}" alt="student-pic" class="student-pic">
+    </div>
+    <div class="student-name">${setName}</div>
+  </div>`;
+  html += todo;
+}
+
+function displayTeachers(teacherName, teacherPic, folder) {
+  todo = `<div>
+  <div class="child-teacher-container">
+    <img src="${folder}/${teacherPic}" alt="student-pic" class="teacher-pic">
+  </div>
+  <div class="teacher-name">${teacherName}</div>
+  </div>`;
+  html += todo
+}
+
+
+function displayMemories(showMemories, folder) {
+  todo = `<img src="${folder}/${showMemories}" alt="memory" class="memory-image">`;
+  html += todo;
+}
+
+
+
+//-----------------Sections-1---------------------------------
+
+for (let i = 0; i < section1Boys.length; i++) {
+  let setName = section1Boys[i].name;
+  let setpic = section1Boys[i].pic;
+  let folder = "section1Boys"
+  displayStudent(setName, setpic, folder)
+}
+document.querySelector(".section1Boys").innerHTML = html
+html = "";
+
+for (let i = 0; i < section1Girls.length; i++) {
+  let setName = section1Girls[i].name;
+  let setpic = section1Girls[i].pic;
+  let folder = "section1Girls"
+  displayStudent(setName, setpic, folder)
+}
+document.querySelector(".section1Girls").innerHTML = html
+html = "";
+
+//-----------------Sections-2---------------------------------
+
+for (let i = 0; i < section2Boys.length; i++) {
+  let setName = section2Boys[i].name;
+  let setpic = section2Boys[i].pic;
+  let folder = "section1Boys"
+  displayStudent(setName, setpic, folder)
+}
+document.querySelector(".section2Boys").innerHTML = html
+html = "";
+
+for (let i = 0; i < section1Girls.length; i++) {
+  let setName = section2Girls[i].name;
+  let setpic = section2Girls[i].pic;
+  let folder = "section2Girls"
+  displayStudent(setName, setpic, folder)
+}
+document.querySelector(".section2Girls").innerHTML = html
+html = "";
+
+//-----------------Sections-3---------------------------------
+
+for (let i = 0; i < section3Boys.length; i++) {
+  let setName = section3Boys[i].name;
+  let setpic = section3Boys[i].pic;
+  let folder = "section3Boys"
+  displayStudent(setName, setpic, folder)
+}
+document.querySelector(".section3Boys").innerHTML = html
+html = "";
+
+for (let i = 0; i < section3Girls.length; i++) {
+  let setName = section3Girls[i].name;
+  let setpic = section3Girls[i].pic;
+  let folder = "section3Girls"
+  displayStudent(setName, setpic, folder)
+}
+document.querySelector(".section3Girls").innerHTML = html
+html = "";
+
+//-----------------Sections-4---------------------------------
+
+for (let i = 0; i < section4Boys.length; i++) {
+  let setName = section4Boys[i].name;
+  let setpic = section4Boys[i].pic;
+  let folder = "section4Boys"
+  displayStudent(setName, setpic, folder)
+}
+document.querySelector(".section4Boys").innerHTML = html
+html = "";
+
+for (let i = 0; i < section4Girls.length; i++) {
+  let setName = section4Girls[i].name;
+  let setpic = section4Girls[i].pic;
+  let folder = "section4Girls"
+  displayStudent(setName, setpic, folder)
+}
+document.querySelector(".section4Girls").innerHTML = html
+html = "";
+
+//-----------------Sections-5---------------------------------
+
+for (let i = 0; i < section5Boys.length; i++) {
+  let setName = section5Boys[i].name;
+  let setpic = section5Boys[i].pic;
+  let folder = "section5Boys"
+  displayStudent(setName, setpic, folder)
+}
+document.querySelector(".section5Boys").innerHTML = html
+html = "";
+
+for (let i = 0; i < section5Girls.length; i++) {
+  let setName = section5Girls[i].name;
+  let setpic = section5Girls[i].pic;
+  let folder = "section5Girls"
+  displayStudent(setName, setpic, folder)
+}
+document.querySelector(".section5Girls").innerHTML = html
+html = "";
+
+//-----------------Sections-6---------------------------------
+
+for (let i = 0; i < section6Boys.length; i++) {
+  let setName = section6Boys[i].name;
+  let setpic = section6Boys[i].pic;
+  let folder = "section6Boys"
+  displayStudent(setName, setpic, folder)
+}
+document.querySelector(".section6Boys").innerHTML = html
+html = "";
+
+for (let i = 0; i < section6Girls.length; i++) {
+  let setName = section6Girls[i].name;
+  let setpic = section6Girls[i].pic;
+  let folder = "section6Girls"
+  displayStudent(setName, setpic, folder)
+}
+document.querySelector(".section6Girls").innerHTML = html
+html = "";
+
+
+
+//-----------------Teachers----------------------------------
+
+for (let i = 0 ; i < teachers.length; i++) {
+  let teacherName = teachers[i].name;
+  let teacherPic = teachers[i].pic;
+  let folder = "teachers";
+  displayTeachers(teacherName, teacherPic, folder);
+}
+document.querySelector(".teacher-container").innerHTML = html;
+html = "";
+
+//-----------------memories----------------------------------
+
+for (let i = 0 ; i < memories.length; i++) {
+  let showMemories = memories[i];
+  let folder = "memories";
+  displayMemories(showMemories, folder);
+}
+document.querySelector(".memory-container").innerHTML = html;
+
+
+
+
+
+
+
+document.querySelectorAll('img').forEach(image =>{
+  image.onclick = () => {
+    document.querySelector('.pop-up').style.display = 'block';
+    document.querySelector('img').src = image.getAttribute('src');
+  }
+});
+
+document.querySelector('span').onclick = () => {
+  document.querySelector('.pop-up').style.display = 'none';
+}
+
+const gotop = document.querySelector('.scroll-top-btn');
+
+window.addEventListener('scroll', checkHeigth)
+
+function checkHeigth() {
+  if (window.scrollY > 100) {
+    gotop.style.display = 'flex';
+  }else {
+    gotop.style.display = 'none';
+  }
+}
+gotop.addEventListener('click', () => {
+  document.querySelector('.pop-up').style.display = 'none';
+  window.scrollTo({
+    top: 0
+  })
+})
